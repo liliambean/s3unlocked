0a31b5e3e839eb47eb8d6afa19cab2b2dbd17f8f
Liliam Bronze <liliambronze@gmail.com>
2022-02-06 20:51:34 +0000

    Animate rings via DMA

---
+++ General/Sprites/Ring/Anim - Slots Ring.asm
@@ -0,0 +1,2 @@
+		dc.w byte_1AA68-Ani_SlotRing
+byte_1AA68:	dc.b    0,   0,   1,   2,   3,   8,   9,  $A,  $B, $FF 
---
+++ General/Sprites/Ring/Map - Slots Ring.asm
@@ -0,0 +1,40 @@
+		dc.w Map_SlotRing1-Map_SlotRing
+		dc.w Map_SlotRing2-Map_SlotRing
+		dc.w Map_SlotRing3-Map_SlotRing
+		dc.w Map_SlotRing4-Map_SlotRing
+		dc.w Map_SlotRingSparkle1-Map_SlotRing
+		dc.w Map_SlotRingSparkle2-Map_SlotRing
+		dc.w Map_SlotRingSparkle3-Map_SlotRing
+		dc.w Map_SlotRingSparkle4-Map_SlotRing
+		dc.w Map_SlotRing5-Map_SlotRing
+		dc.w Map_SlotRing6-Map_SlotRing
+		dc.w Map_SlotRing7-Map_SlotRing
+		dc.w Map_SlotRing8-Map_SlotRing
+Map_SlotRing1:	dc.w 1
+		dc.b  $F8,   5,   0,   4, $FF, $F8
+Map_SlotRing2:	dc.w 1
+		dc.b  $F8,   5,   0,  $C, $FF, $F8
+Map_SlotRing3:	dc.w 1
+		dc.b  $F8,   5,   0, $14, $FF, $F8
+Map_SlotRing4:	dc.w 1
+		dc.b  $F8,   5,   0, $1C, $FF, $F8
+Map_SlotRing5:	dc.w 1
+		dc.b  $F8,   5,   0, $24, $FF, $F8
+Map_SlotRing6:	dc.w 1
+		dc.b  $F8,   5,   0, $2C, $FF, $F8
+Map_SlotRing7:	dc.w 1
+		dc.b  $F8,   5,   0, $34, $FF, $F8
+Map_SlotRing8:	dc.w 1
+		dc.b  $F8,   5,   0, $3C, $FF, $F8
+Map_SlotRingSparkle1:
+		dc.w 1
+		dc.b  $F8,   5,   1, $28, $FF, $F8
+Map_SlotRingSparkle2:
+		dc.w 1
+		dc.b  $F8,   5, $19, $28, $FF, $F8
+Map_SlotRingSparkle3:
+		dc.w 1
+		dc.b  $F8,   5,   9, $28, $FF, $F8
+Map_SlotRingSparkle4:
+		dc.w 1
+		dc.b  $F8,   5, $11, $28, $FF, $F8
