089b00222179bccdb7b5c37f3085a20895fc46d8
Liliam Bronze <liliambronze@gmail.com>
2022-11-26 02:42:08 +0000

    Rename museum mode

---
+++ General/Sprites/Ring/Map - Photo Piece.asm
@@ -0,0 +1,29 @@
+		dc.w Map_PhotoPieceMain-Map_PhotoPiece
+		dc.w Map_PhotoPieceMain-Map_PhotoPiece
+		dc.w Map_PhotoPieceMain-Map_PhotoPiece
+		dc.w Map_PhotoPieceMain-Map_PhotoPiece
+		dc.w Map_PhotoPieceSparkle1-Map_PhotoPiece
+		dc.w Map_PhotoPieceSparkle2-Map_PhotoPiece
+		dc.w Map_PhotoPieceSparkle3-Map_PhotoPiece
+		dc.w Map_PhotoPieceSparkle4-Map_PhotoPiece
+		dc.w Map_PhotoPieceBlank-Map_PhotoPiece
+Map_PhotoPieceMain:
+		dc.w 2
+		dc.b  $FA,   5, $20, $D8, $FF, $F8
+		dc.b   $A,   4,   0,   6, $FF, $F8
+Map_PhotoPieceSparkle1:
+		dc.w 2
+		dc.b  $FA,   5, $20, $E4, $FF, $F8
+		dc.b   $A,   4,   0,   6, $FF, $F8
+Map_PhotoPieceSparkle2:
+		dc.w 2
+		dc.b  $FA,   5, $38, $E4, $FF, $F8
+		dc.b   $A,   4,   0,   6, $FF, $F8
+Map_PhotoPieceSparkle3:
+		dc.w 1
+		dc.b  $FA,   5, $28, $E4, $FF, $F8
+Map_PhotoPieceSparkle4:
+		dc.w 1
+		dc.b  $FA,   5, $30, $E4, $FF, $F8
+Map_PhotoPieceBlank:
+		dc.w 0
--- sonic3k.macros.asm
+++ sonic3k.macros.asm
@@ -137,8 +137,8 @@ levselstr macro str
 	codepage LEVELSELECT
 	charset '0','9', 16
 	charset 'A','Z', 30
-	charset '<', 56
-	charset '>', 57
+	charset '<', 26
+	charset '>', 27
 	charset ' ',  0
 	restore
 
