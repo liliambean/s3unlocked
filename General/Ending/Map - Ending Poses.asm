eedeca2181a6cb114b46d65935d1c6bca3611694
Liliam Bronze <liliambronze@gmail.com>
2022-12-07 04:58:36 +0000

    Split off character ending poses

---
+++ General/Ending/Map - Ending Poses.asm
@@ -0,0 +1,169 @@
+Map_607E6:	dc.w word_607F0-Map_607E6
+		dc.w word_60816-Map_607E6
+		dc.w word_608D4-Map_607E6
+		dc.w word_608EE-Map_607E6
+		dc.w word_60A5A-Map_607E6
+		dc.w word_60A68-Map_607E6
+		dc.w Map_EndingPoses_Amy1-Map_607E6
+		dc.w Map_EndingPoses_Amy2-Map_607E6
+		dc.w Map_EndingPoses_Mighty1-Map_607E6
+		dc.w Map_EndingPoses_Mighty2-Map_607E6
+		dc.w Map_EndingPoses_Ray1-Map_607E6
+		dc.w Map_EndingPoses_Ray2-Map_607E6
+		dc.w word_607F0-Map_607E6
+		dc.w word_60816-Map_607E6
+		dc.w word_60A52-Map_607E6
+		dc.w word_60866-Map_607E6
+word_607F0:	dc.w 6			
+		dc.b  $DC,  $F,	  0,   0, $FF, $F0
+		dc.b  $E4,   1,	  0, $10,   0, $10
+		dc.b  $E4,   2,	  0, $12, $FF, $E8
+		dc.b  $FC,  $E,	  0, $15, $FF, $E8
+		dc.b  $FC,   1,	  0, $21,   0,	 8
+		dc.b  $14,   5,	  0, $23, $FF, $F0
+word_60816:	dc.w $D			
+		dc.b  $C0,  $F,	  8, $27,   0,	 4
+		dc.b  $C0,  $B,	  8, $37, $FF, $EC
+		dc.b  $C8,   5,	  8, $43, $FF, $DC
+		dc.b  $D8,  $B,	  8, $47, $FF, $D4
+		dc.b  $E0,   6,	  8, $53,   0, $1C
+		dc.b  $E0,  $F,	  8, $59, $FF, $FC
+		dc.b  $E0,   7,	  8, $69, $FF, $EC
+		dc.b  $F8,   4,	  8, $71, $FF, $DC
+		dc.b	0,  $F,	  8, $73, $FF, $E4
+		dc.b  $20,   6,	  8, $83, $FF, $F4
+		dc.b	0,  $A,	  8, $89,   0,	 4
+		dc.b  $18,  $A,	  8, $92,   0,	 4
+		dc.b  $30,   9,	  8, $9B,   0,	$C
+word_60866:	dc.w $12		
+		dc.b  $BC,  $D,	  0,   0, $FF, $F0
+		dc.b  $CC,  $F,	  0,   8, $FF, $D0
+		dc.b  $CC,   7,	  0, $18, $FF, $F0
+		dc.b  $CC,   8,	  0, $20,   0,	 8
+		dc.b  $D4,  $A,	  0, $23,   0,	 0
+		dc.b  $E4,   7,	  0, $2C, $FF, $C0
+		dc.b  $EC,  $F,	  0, $34, $FF, $D0
+		dc.b  $EC,  $F,	  0, $44, $FF, $F0
+		dc.b  $EC,   3,	  0, $54,   0, $10
+		dc.b  $FC,  $F,	  0, $58,   0, $18
+		dc.b   $C,   1,	  0, $68,   0, $38
+		dc.b  $1C,   8,	  0, $6A,   0, $18
+		dc.b   $C,  $F,	  0, $6D, $FF, $F8
+		dc.b   $C,  $F,	  0, $7D, $FF, $D8
+		dc.b	4,   7,	  0, $8D, $FF, $C0
+		dc.b   $C,   1,	  0, $95, $FF, $D0
+		dc.b  $24,  $F,	  0, $97, $FF, $C0
+		dc.b  $2C,   0,	  0, $A7, $FF, $E0
+word_608D4:	dc.w 4			
+		dc.b  $E8,   8,	  8,   0, $FF, $FC
+		dc.b  $F0,  $E,	  8,   3, $FF, $F4
+		dc.b	8,   9,	  8,  $F, $FF, $FC
+		dc.b  $F8,   2,	  8, $15, $FF, $EC
+word_608EE:	dc.w $C			
+		dc.b  $E0,  $F,	  8, $18,   0,	 0
+		dc.b  $E0,  $F,	  8, $28, $FF, $E0
+		dc.b	0,  $F,	  8, $38,   0,	 0
+		dc.b	0,  $F,	  8, $48, $FF, $E0
+		dc.b  $D0,   5,	  8, $58,   0,	 8
+		dc.b  $D8,   4,	  8, $5C, $FF, $F8
+		dc.b  $F0,   6,	  8, $5E,   0, $20
+		dc.b	8,   1,	  8, $64,   0, $20
+		dc.b  $20,   5,	  8, $66,   0, $10
+		dc.b  $20,  $D,	  8, $6A, $FF, $E8
+		dc.b  $F0,   6,	  8, $72, $FF, $D0
+		dc.b	8,   5,	  8, $78, $FF, $D0
+Map_EndingPoses_Amy1:
+		dc.w 5			
+		dc.b  $E0,  $F,	  0,   0, $FF, $F0
+		dc.b  $E0,   2,	  0, $10,   0, $10
+		dc.b  $EC,   2,	  0, $13, $FF, $E8
+		dc.b	0,  $D,	  0, $16, $FF, $F0
+		dc.b  $10,   5,	  0, $1E, $FF, $F0
+Map_EndingPoses_Amy2:
+		dc.w 9			
+		dc.b  $CC,  $E,	  0, $22, $FF, $E6
+		dc.b  $CC,  $E,	  0, $2E,   0,	 6
+		dc.b  $E4,  $F,	  0, $3A, $FF, $DA
+		dc.b  $E4,  $F,	  0, $4A, $FF, $FA
+		dc.b  $E4,   1,	  0, $5A,   0, $1A
+		dc.b	4,   9,	  0, $5C, $FF, $DE
+		dc.b	4,  $D,	  0, $62, $FF, $F6
+		dc.b  $14,   7,	  0, $6A, $FF, $E2
+		dc.b  $14,  $E,	  0, $72, $FF, $F2
+Map_EndingPoses_Mighty1:
+		dc.w $A			
+		dc.b  $D8,   0,	  0,   0, $FF, $FA
+		dc.b  $E0,  $C,	  0,   1, $FF, $F6
+		dc.b  $E8,   4,	  0,   5,   0,	$E
+		dc.b  $F0,   0,	  0,   7,   0,	$E
+		dc.b  $E8,  $D,	  0,   8, $FF, $EE
+		dc.b  $F8,  $C,	  0, $10, $FF, $F6
+		dc.b  $F8,   4,	  0, $14, $FF, $E6
+		dc.b	0,  $D,	  0, $16, $FF, $FA
+		dc.b	0,  $B,	  0, $1E, $FF, $E2
+		dc.b  $20,   0,	  0, $2A, $FF, $F2
+Map_EndingPoses_Mighty2:
+		dc.w $14		
+		dc.b  $C4,   8,	  0, $2B,   0, $24
+		dc.b  $CC,  $F,	  0, $2E,   0, $1C
+		dc.b  $DC,   1,	  0, $3E,   0, $3C
+		dc.b  $CC,  $F,	  0, $40, $FF, $FC
+		dc.b  $CC,   0,	  0, $50, $FF, $F4
+		dc.b  $BC,   5,	  0, $51,   0,	 0
+		dc.b  $D4,  $E,	  0, $55, $FF, $DC
+		dc.b  $E4,   0,	  0, $61, $FF, $D4
+		dc.b  $CC,  $E,	  0, $62, $FF, $BC
+		dc.b  $C4,   0,	  0, $6E, $FF, $C4
+		dc.b  $EC,  $F,	  0, $6F,   0,	$E
+		dc.b  $EC,  $F,	  0, $7F, $FF, $EE
+		dc.b  $EC,   7,	  0, $8F, $FF, $DE
+		dc.b   $C,  $F,	  0, $97, $FF, $C6
+		dc.b  $24,   0,	  0, $A7, $FF, $E6
+		dc.b   $C,  $E,	  0, $A8, $FF, $E6
+		dc.b   $C,   1,	  0, $B4,   0,	 6
+		dc.b  $1C,   4,	  0, $B6,   0,	 6
+		dc.b  $24,  $F,	  0, $B8,   0,	 4
+		dc.b  $24,   6,	  0, $C8,   0, $24
+Map_EndingPoses_Ray1:
+		dc.w 6			
+		dc.b  $D8,  $F,	  0,   0, $FF, $F0
+		dc.b  $F8,  $E,	  0, $10, $FF, $F0
+		dc.b  $F8,   5,	  0, $1C, $FF, $E0
+		dc.b  $F4,   5,	  0, $20,   0, $10
+		dc.b  $10,   5,	  0, $24, $FF, $F8
+		dc.b  $20,   0,	  0, $28, $FF, $F8
+Map_EndingPoses_Ray2:
+		dc.w $13		
+		dc.b  $B4,   8,	  0, $29, $FF, $F1
+		dc.b  $B4,   4,	  0, $2C,   0,	 9
+		dc.b  $BC,  $D,	  0, $2E, $FF, $E9
+		dc.b  $BC,   9,	  0, $36,   0,	 9
+		dc.b  $CC,  $F,	  0, $3C, $FF, $E9
+		dc.b  $CC,  $F,	  0, $4C,   0,	 9
+		dc.b  $CC,   7,	  0, $5C,   0, $29
+		dc.b  $EC,  $C,	  0, $64, $FF, $F2
+		dc.b  $EC,  $C,	  0, $68,   0, $12
+		dc.b  $F4,  $F,	  0, $6C,   0,	$F
+		dc.b  $F4,  $F,	  0, $7C, $FF, $EF
+		dc.b  $F4,  $F,	  0, $8C, $FF, $CF
+		dc.b  $D4,  $B,	  0, $9C, $FF, $C6
+		dc.b  $14,  $C,	  0, $A8, $FF, $D1
+		dc.b  $14,  $E,	  0, $AC, $FF, $F1
+		dc.b  $14,   7,	  0, $B8,   0, $11
+		dc.b  $34,   0,	  0, $C0,   0, $19
+		dc.b  $24,   0,	  0, $C1, $FF, $E9
+		dc.b  $2C,  $B,	  0, $C2, $FF, $E7
+word_60A52:	dc.w 1			
+		dc.b  $F8,   5,	  0,   0, $FF, $F8
+word_60A5A:	dc.w 2			
+		dc.b  $F0,  $F,	  0,   4, $FF, $F0
+		dc.b  $10,   4,	  0, $14,   0,	 0
+word_60A68:	dc.w 8			
+		dc.b  $CC,  $F,	  0, $16,   0,	 0
+		dc.b  $CC,   5,	  0, $26, $FF, $F0
+		dc.b  $DC,  $F,	  0, $2A, $FF, $E0
+		dc.b  $EC,  $D,	  0, $3A,   0,	 0
+		dc.b  $FC,  $F,	  0, $42, $FF, $E0
+		dc.b  $FC,   6,	  0, $52,   0,	 0
+		dc.b  $14,   9,	  0, $58,   0,	 0
+		dc.b  $1C,   6,	  0, $5E, $FF, $F0
