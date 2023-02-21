c4349dccb4bf4907d217f1280732c9422c66c228
Liliam Bronze <liliambronze@gmail.com>
2023-02-21 20:37:42 +0000

    Use Mecha Sonic head for bosses under... circumstances

--- General/Sprites/Mecha Sonic/Map - Mecha Sonic Head.asm
+++ General/Sprites/Mecha Sonic/Map - Mecha Sonic Head.asm
@@ -1,4 +1,9 @@
 Map_681FC:		dc.w word_681FE-Map_681FC
+			dc.w Map_MechaSonicHead2-Map_681FC
 word_681FE:	dc.w 2			; DATA XREF: ROM:000681FCo
 		dc.b  $F4,  $E,	  0,   0, $FF, $EC
 		dc.b  $FC,   1,	  0,  $C,   0,	$C
+Map_MechaSonicHead2:
+		dc.w 2
+		dc.b  $F4,  $E,	  0,  $E, $FF, $EC
+		dc.b  $FC,   1,	  0, $1A,   0,	$C
