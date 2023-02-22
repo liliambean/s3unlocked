c79e9d80b3c58933d90df7b7ff2380dc2ab605c1
Liliam Bronze <liliambronze@gmail.com>
2023-02-22 20:15:29 +0000

    Display correct boss heads in AIZ2

--- Levels/AIZ/Misc Object Data/Map - Act 2 Boss Small.asm
+++ Levels/AIZ/Misc Object Data/Map - Act 2 Boss Small.asm
@@ -1,8 +1,8 @@
 Map_AIZ2BossSmall_:dc.w word_23C266-Map_AIZ2BossSmall_
 word_23C266:     dc.w 6
-		dc.b $E4, $E,$20,$86,$FF,$F0
-		dc.b $F4,  0,$20,$92,$FF,$E8
-		dc.b $F4,  0,$20,$93,  0,$10
+		dc.b $E4, $D,$20,$86,$FF,$F0
+		dc.b $F4,  8,$20,$8E,$FF,$E8
+		dc.b $F4,  8,$20,$91,  0,  0
 		dc.b $FC, $E,$20,$94,$FF,$E0
 		dc.b $FC, $E,$20,$A0,  0,  0
 		dc.b $14, $C,$20,$AC,$FF,$F0
