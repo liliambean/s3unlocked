f1673dcc5586da09b44fb4956647879bdf6112e1
Liliam Bronze <liliambronze@gmail.com>
2023-02-25 01:31:04 +0000

    Polish up Death Egg Robot intro sequence

---
+++ Levels/SSZ/Misc Object Data/Map - SSZ2 Extra.asm
@@ -0,0 +1,15 @@
+		dc.w Map_MechaSonicIntroFire1-Map_SSZ2Extra
+		dc.w Map_MechaSonicIntroFire2-Map_SSZ2Extra
+		dc.w Map_SSZ2EggRoboPanel-Map_SSZ2Extra
+Map_MechaSonicIntroFire1:
+		dc.w 2
+		dc.b  $F4,   2, $20,   0, $FF, $F7
+		dc.b  $F4,   2, $20,   0,   0,   1
+Map_MechaSonicIntroFire2:
+		dc.w 2
+		dc.b  $F4,   0, $20,   3, $FF, $F7
+		dc.b  $F4,   0, $20,   3,   0,   1
+Map_SSZ2EggRoboPanel:
+		dc.w 2
+		dc.b  $EC,   9,   0,   4, $FF, $F4
+		dc.b  $FC,  $A,   0,  $A, $FF, $F4
