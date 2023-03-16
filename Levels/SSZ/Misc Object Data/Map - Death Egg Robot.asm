878d1a8607fd790953a90503bde97df84b19dc69
Liliam Bronze <liliambronze@gmail.com>
2023-03-16 00:19:53 +0000

    Use S3 graphics for Death Egg Robot

--- Levels/SSZ/Misc Object Data/Map - Death Egg Robot.asm
+++ Levels/SSZ/Misc Object Data/Map - Death Egg Robot.asm
@@ -1,145 +1,158 @@
-		dc.w Map_DeathEggRobot_2E-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_34-Map_DeathEggRobot
 		dc.w Map_DeathEggRobot_36-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_4A-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_44-Map_DeathEggRobot
 		dc.w Map_DeathEggRobot_5E-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_9C-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_72-Map_DeathEggRobot
 		dc.w Map_DeathEggRobot_A4-Map_DeathEggRobot
 		dc.w Map_DeathEggRobot_AC-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_BA-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_C2-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_CA-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_D2-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_CC-Map_DeathEggRobot
 		dc.w Map_DeathEggRobot_DA-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_100-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_108-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_110-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_118-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_120-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_152-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_184-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_1B6-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_1E8-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_1F0-Map_DeathEggRobot
-		dc.w Map_DeathEggRobot_1F8-Map_DeathEggRobot
-Map_DeathEggRobot_2E:
-		dc.w 1
-		dc.b  $F0,  $B,   0,   0,   0,  $C
+		dc.w Map_DeathEggRobot_E2-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_EA-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_F2-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_FA-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_12C-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_15E-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_190-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_1C2-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_1F4-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_1FC-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_204-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_20C-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_214-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_21C-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_224-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_232-Map_DeathEggRobot
+		dc.w Map_DeathEggRobot_240-Map_DeathEggRobot
+Map_DeathEggRobot_34:
+		dc.w 0
 Map_DeathEggRobot_36:
-		dc.w 3
-		dc.b  $F0,  $D,   0,  $C, $FF, $EC
-		dc.b    0,   9,   0, $14, $FF, $F4
-		dc.b  $F8,  $A,   0, $1A,   0,  $C
-Map_DeathEggRobot_4A:
-		dc.w 3
-		dc.b    8,   0,   0, $23, $FF, $DC
-		dc.b  $F8,  $E,   0, $24, $FF, $E4
-		dc.b  $F8,  $E,   0, $30,   0,   4
+		dc.w 2
+		dc.b  $F0,   0, $20,   0,   0,  $E
+		dc.b  $F8,  $A, $20,   1,   0,  $B
+Map_DeathEggRobot_44:
+		dc.w 4
+		dc.b  $F0,   9, $20,  $A, $FF, $F1
+		dc.b  $F8,   4, $20, $10,   0,   9
+		dc.b    0,  $D, $20, $12, $FF, $F9
+		dc.b    0,   5, $20, $1A,   0, $19
 Map_DeathEggRobot_5E:
-		dc.w $A
-		dc.b  $DC,  $F, $20, $3C, $FF, $D4
-		dc.b  $DC,  $F, $20, $4C, $FF, $F4
-		dc.b  $DC,  $B, $20, $5C,   0, $14
-		dc.b  $DC,  $B, $20, $68,   0, $2C
-		dc.b  $FC,  $F, $20, $74, $FF, $D4
-		dc.b  $FC,  $F, $20, $84, $FF, $F4
-		dc.b  $FC,  $B, $20, $94,   0, $14
-		dc.b  $FC,   9, $20, $A0,   0, $2C
-		dc.b  $1C,  $C, $20, $A6, $FF, $E4
-		dc.b  $1C,  $C, $20, $AA,   0,   4
-Map_DeathEggRobot_9C:
-		dc.w 1
-		dc.b  $F0,  $F, $20, $AE, $FF, $F0
+		dc.w 3
+		dc.b    8,   0, $20, $1E, $FF, $DC
+		dc.b  $F8,  $E, $20, $1F, $FF, $E4
+		dc.b  $F8,  $E, $20, $2B,   0,   4
+Map_DeathEggRobot_72:
+		dc.w 8
+		dc.b  $DC,   3, $20, $37, $FF, $DC
+		dc.b  $FC,   1, $20, $3B, $FF, $DC
+		dc.b  $DC,  $F, $20, $3D, $FF, $E4
+		dc.b  $FC,  $F, $20, $4D, $FF, $E4
+		dc.b  $DC,  $F, $20, $5D,   0,   4
+		dc.b  $FC,  $F, $20, $6D,   0,   4
+		dc.b  $D4,  $F, $20, $7D,   0, $24
+		dc.b  $F4,  $F, $20, $8D,   0, $24
 Map_DeathEggRobot_A4:
 		dc.w 1
-		dc.b  $F4,  $A, $20, $BE, $FF, $F4
+		dc.b  $F8,   9, $20, $9D, $FF, $F4
 Map_DeathEggRobot_AC:
+		dc.w 5
+		dc.b  $18,   0, $20, $A3, $FF, $D4
+		dc.b  $E0,  $B, $20, $A4, $FF, $E4
+		dc.b    0,  $F, $20, $B0, $FF, $DC
+		dc.b  $F0,  $B, $20, $C0, $FF, $FC
+		dc.b  $10,  $D, $20, $CC, $FF, $FC
+Map_DeathEggRobot_CC:
 		dc.w 2
-		dc.b  $F4,  $E, $20, $C7,   0,   0
-		dc.b  $F4,  $E, $20, $D3, $FF, $E0
-Map_DeathEggRobot_BA:
-		dc.w 1
-		dc.b  $F0,  $F, $20, $E2, $FF, $F0
-Map_DeathEggRobot_C2:
-		dc.w 1
-		dc.b  $F0,  $F, $20, $E2, $FF, $F0
-Map_DeathEggRobot_CA:
-		dc.w 1
-		dc.b  $F0,  $F, $20, $E2, $FF, $F0
-Map_DeathEggRobot_D2:
-		dc.w 1
-		dc.b  $F0,  $F, $20, $E2, $FF, $F0
+		dc.b  $F4,  $E, $20, $D4, $FF, $E0
+		dc.b  $F4,  $E, $20, $E0,   0,   0
 Map_DeathEggRobot_DA:
-		dc.w 6
-		dc.b  $E0,   6, $20, $F2, $FF, $EB
-		dc.b  $E8,   9, $20, $F8, $FF, $FB
-		dc.b  $F8,   6, $20, $FE, $FF, $EE
-		dc.b  $10,  $D, $21,   4, $FF, $E0
-		dc.b  $F8,  $E, $21,  $C, $FF, $FE
-		dc.b  $10,  $D, $21, $18,   0,   0
-Map_DeathEggRobot_100:
 		dc.w 1
-		dc.b  $F4,   6, $21, $20, $FF, $F8
-Map_DeathEggRobot_108:
+		dc.b  $FC,   8, $20, $EC, $FF, $F4
+Map_DeathEggRobot_E2:
 		dc.w 1
-		dc.b  $F4,   5, $21, $26, $FF, $F8
-Map_DeathEggRobot_110:
+		dc.b  $F0,  $F, $20, $EF, $FF, $F0
+Map_DeathEggRobot_EA:
 		dc.w 1
-		dc.b  $F0,  $F,   1, $2B, $FF, $F0
-Map_DeathEggRobot_118:
+		dc.b    4,  $F, $20, $FF, $FF, $F0
+Map_DeathEggRobot_F2:
 		dc.w 1
-		dc.b  $FC,   0,   1, $3B, $FF, $FC
-Map_DeathEggRobot_120:
+		dc.b    4,   9, $21,  $F, $FF, $F4
+Map_DeathEggRobot_FA:
 		dc.w 8
-		dc.b  $F0,   5, $21, $3C, $FF, $F0
-		dc.b  $F0,   5, $29, $3C,   0,   0
-		dc.b    0,   5, $31, $3C, $FF, $F0
-		dc.b    0,   5, $39, $3C,   0,   0
-		dc.b  $E0,   0, $21, $44, $FF, $E0
-		dc.b  $E0,   0, $29, $44,   0, $18
-		dc.b  $18,   0, $31, $44, $FF, $E0
-		dc.b  $18,   0, $39, $44,   0, $18
-Map_DeathEggRobot_152:
+		dc.b  $F0,   5, $21, $15, $FF, $F0
+		dc.b  $F0,   5, $29, $15,   0,   0
+		dc.b    0,   5, $31, $15, $FF, $F0
+		dc.b    0,   5, $39, $15,   0,   0
+		dc.b  $C8,   0, $21, $19, $FF, $C8
+		dc.b  $C8,   0, $29, $19,   0, $30
+		dc.b  $30,   0, $31, $19, $FF, $C8
+		dc.b  $30,   0, $39, $19,   0, $30
+Map_DeathEggRobot_12C:
 		dc.w 8
-		dc.b  $F0,   5, $21, $3C, $FF, $F0
-		dc.b  $F0,   5, $29, $3C,   0,   0
-		dc.b    0,   5, $31, $3C, $FF, $F0
-		dc.b    0,   5, $39, $3C,   0,   0
-		dc.b  $D8,   0, $21, $45, $FF, $D8
-		dc.b  $D8,   0, $29, $45,   0, $20
-		dc.b  $20,   0, $31, $45, $FF, $D8
-		dc.b  $20,   0, $39, $45,   0, $20
-Map_DeathEggRobot_184:
+		dc.b  $F0,   5, $21, $15, $FF, $F0
+		dc.b  $F0,   5, $29, $15,   0,   0
+		dc.b    0,   5, $31, $15, $FF, $F0
+		dc.b    0,   5, $39, $15,   0,   0
+		dc.b  $D0,   0, $21, $1A, $FF, $D0
+		dc.b  $D0,   0, $29, $1A,   0, $28
+		dc.b  $28,   0, $31, $1A, $FF, $D0
+		dc.b  $28,   0, $39, $1A,   0, $28
+Map_DeathEggRobot_15E:
 		dc.w 8
-		dc.b  $F0,   5, $21, $3C, $FF, $F0
-		dc.b  $F0,   5, $29, $3C,   0,   0
-		dc.b    0,   5, $31, $3C, $FF, $F0
-		dc.b    0,   5, $39, $3C,   0,   0
-		dc.b  $D0,   0, $21, $46, $FF, $D0
-		dc.b  $D0,   0, $29, $46,   0, $28
-		dc.b  $28,   0, $31, $46, $FF, $D0
-		dc.b  $28,   0, $39, $46,   0, $28
-Map_DeathEggRobot_1B6:
+		dc.b  $F0,   5, $21, $15, $FF, $F0
+		dc.b  $F0,   5, $29, $15,   0,   0
+		dc.b    0,   5, $31, $15, $FF, $F0
+		dc.b    0,   5, $39, $15,   0,   0
+		dc.b  $D8,   0, $21, $1B, $FF, $D8
+		dc.b  $D8,   0, $29, $1B,   0, $20
+		dc.b  $20,   0, $31, $1B, $FF, $D8
+		dc.b  $20,   0, $39, $1B,   0, $20
+Map_DeathEggRobot_190:
 		dc.w 8
-		dc.b  $F0,   5, $21, $3C, $FF, $F0
-		dc.b  $F0,   5, $29, $3C,   0,   0
-		dc.b    0,   5, $31, $3C, $FF, $F0
-		dc.b    0,   5, $39, $3C,   0,   0
-		dc.b  $C8,   0, $21, $2A, $FF, $C8
-		dc.b  $C8,   0, $29, $2A,   0, $30
-		dc.b  $30,   0, $31, $2A, $FF, $C8
-		dc.b  $30,   0, $39, $2A,   0, $30
-Map_DeathEggRobot_1E8:
+		dc.b  $F0,   5, $21, $15, $FF, $F0
+		dc.b  $F0,   5, $29, $15,   0,   0
+		dc.b    0,   5, $31, $15, $FF, $F0
+		dc.b    0,   5, $39, $15,   0,   0
+		dc.b  $E0,   0, $21, $1C, $FF, $E0
+		dc.b  $E0,   0, $29, $1C,   0, $18
+		dc.b  $18,   0, $31, $1C, $FF, $E0
+		dc.b  $18,   0, $39, $1C,   0, $18
+Map_DeathEggRobot_1C2:
+		dc.w 8
+		dc.b  $F0,   5, $21, $15, $FF, $F0
+		dc.b  $F0,   5, $29, $15,   0,   0
+		dc.b    0,   5, $31, $15, $FF, $F0
+		dc.b    0,   5, $39, $15,   0,   0
+		dc.b  $E8,   0, $21, $1C, $FF, $E8
+		dc.b  $E8,   0, $29, $1C,   0, $10
+		dc.b  $10,   0, $31, $1C, $FF, $E8
+		dc.b  $10,   0, $39, $1C,   0, $10
+Map_DeathEggRobot_1F4:
 		dc.w 1
-		dc.b  $F8,   5,   1, $40, $FF, $F8
-Map_DeathEggRobot_1F0:
-		dc.w 0
-Map_DeathEggRobot_1F8:
-		dc.w 8
-		dc.b  $F0,   5, $21, $3C, $FF, $F0
-		dc.b  $F0,   5, $29, $3C,   0,   0
-		dc.b    0,   5, $31, $3C, $FF, $F0
-		dc.b    0,   5, $39, $3C,   0,   0
-		dc.b  $E8,   0, $21, $44, $FF, $E8
-		dc.b  $E8,   0, $29, $44,   0, $10
-		dc.b  $10,   0, $31, $44, $FF, $E8
-		dc.b  $10,   0, $39, $44,   0, $10
+		dc.b  $F8,   5,   1, $1D, $FF, $F8
+Map_DeathEggRobot_1FC:
+		dc.w 1
+		dc.b  $F0,   1, $21, $21,   0,   4
+Map_DeathEggRobot_204:
+		dc.w 1
+		dc.b  $F0,   5, $21, $23, $FF, $FC
+Map_DeathEggRobot_20C:
+		dc.w 1
+		dc.b  $F0,   5, $21, $27, $FF, $F8
+Map_DeathEggRobot_214:
+		dc.w 1
+		dc.b  $F0,   5, $29, $23, $FF, $F4
+Map_DeathEggRobot_21C:
+		dc.w 1
+		dc.b  $F0,   1, $29, $21, $FF, $F4
+Map_DeathEggRobot_224:
+		dc.w 2
+		dc.b  $F0,   9, $21, $2B, $FF, $F4
+		dc.b    0,   9, $21, $31, $FF, $F4
+Map_DeathEggRobot_232:
+		dc.w 2
+		dc.b  $F0,   9, $21, $2B, $FF, $F4
+		dc.b    0,   9, $21, $37, $FF, $F4
+Map_DeathEggRobot_240:
+		dc.w 1
+		dc.b  $10,   5, $21, $3D, $FF, $F8
