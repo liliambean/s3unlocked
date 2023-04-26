2ee79c6f8ef90f425c2c3495272cdc5716cfc4f7
Liliam Bronze <liliambronze@gmail.com>
2023-04-26 23:46:38 +0100

    Fix conflict between invincibility and shield sparks

--- General/Sprites/Shields/Map - Invincibility.asm
+++ General/Sprites/Shields/Map - Invincibility.asm
@@ -9,18 +9,18 @@
 		dc.w word_178B6-Map_Invincibility
 word_1787C:	dc.w 0
 word_1787E:	dc.w 1
-		dc.b  $F8,   0,   0,   0, $FF, $FC
+		dc.b  $F8,   0, $18,   5,   0,   0
 word_17886:	dc.w 1
-		dc.b  $F8,   0,   0,   1, $FF, $FC
+		dc.b  $FC,   0,   0,   0, $FF, $FC
 word_1788E:	dc.w 1
-		dc.b  $F8,   1,   0,   2, $FF, $FC
+		dc.b  $FC,   0,   0,   1, $FF, $FC
 word_17896:	dc.w 1
-		dc.b  $F8,   1,   0,   4, $FF, $FC
+		dc.b  $FC,   0,   0,   2, $FF, $FC
 word_1789E:	dc.w 1
-		dc.b  $F8,   1,   0,   6, $FF, $FC
+		dc.b  $FC,   0,   0,   3, $FF, $FC
 word_178A6:	dc.w 1
-		dc.b  $F8,   5,   0,   8, $FF, $F8
+		dc.b  $F8,   5,   0,   4, $FF, $F8
 word_178AE:	dc.w 1
-		dc.b  $F8,   5,   0,  $C, $FF, $F8
+		dc.b  $F8,   5,   0,   8, $FF, $F8
 word_178B6:	dc.w 1
-		dc.b  $F0,  $F,   0, $10, $FF, $F0
+		dc.b  $F0,  $F,   0,  $C, $FF, $F0
