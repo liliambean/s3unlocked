7272712340e44be783f02adc9612450da1712ab4
Liliam Bronze <liliambronze@gmail.com>
2023-06-30 18:19:21 +0100

    Clean up special stage player graphics

--- General/Special Stage/DPLC - Characters.asm
+++ General/Special Stage/DPLC - Characters.asm
@@ -1,259 +1,169 @@
-		dc.w SME_mR7af_FC-PLC_SStageSonic
-		dc.w SME_mR7af_102-PLC_SStageSonic
-		dc.w SME_mR7af_10A-PLC_SStageSonic
-		dc.w SME_mR7af_112-PLC_SStageSonic
-		dc.w SME_mR7af_11A-PLC_SStageSonic
-		dc.w SME_mR7af_122-PLC_SStageSonic
-		dc.w SME_mR7af_12A-PLC_SStageSonic
-		dc.w SME_mR7af_130-PLC_SStageSonic
-		dc.w SME_mR7af_138-PLC_SStageSonic
-		dc.w SME_mR7af_140-PLC_SStageSonic
-		dc.w SME_mR7af_148-PLC_SStageSonic
-		dc.w SME_mR7af_150-PLC_SStageSonic
-		dc.w SME_mR7af_158-PLC_SStageSonic
-		dc.w SME_mR7af_15E-PLC_SStageSonic
-		dc.w SME_mR7af_162-PLC_SStageSonic
-		dc.w SME_mR7af_166-PLC_SStageSonic
+		dc.w PLC_SStageSonic0-PLC_SStageSonic
+		dc.w PLC_SStageSonic1-PLC_SStageSonic
+		dc.w PLC_SStageSonic2-PLC_SStageSonic
+		dc.w PLC_SStageSonic3-PLC_SStageSonic
+		dc.w PLC_SStageSonic4-PLC_SStageSonic
+		dc.w PLC_SStageSonic5-PLC_SStageSonic
+		dc.w PLC_SStageSonic0-PLC_SStageSonic
+		dc.w PLC_SStageSonic1-PLC_SStageSonic
+		dc.w PLC_SStageSonic2-PLC_SStageSonic
+		dc.w PLC_SStageSonic3-PLC_SStageSonic
+		dc.w PLC_SStageSonic4-PLC_SStageSonic
+		dc.w PLC_SStageSonic5-PLC_SStageSonic
+		dc.w PLC_SStageSonicC-PLC_SStageSonic
+		dc.w PLC_SStageSonicD-PLC_SStageSonic
+		dc.w PLC_SStageSonicE-PLC_SStageSonic
+		dc.w PLC_SStageSonicF-PLC_SStageSonic
 PLC_SStageTails:
-		dc.w SME_mR7af_16A-PLC_SStageTails
-		dc.w SME_mR7af_172-PLC_SStageTails
-		dc.w SME_mR7af_17A-PLC_SStageTails
-		dc.w SME_mR7af_182-PLC_SStageTails
-		dc.w SME_mR7af_18A-PLC_SStageTails
-		dc.w SME_mR7af_192-PLC_SStageTails
-		dc.w SME_mR7af_19A-PLC_SStageTails
-		dc.w SME_mR7af_1A2-PLC_SStageTails
-		dc.w SME_mR7af_1AA-PLC_SStageTails
-		dc.w SME_mR7af_1B2-PLC_SStageTails
-		dc.w SME_mR7af_1BA-PLC_SStageTails
-		dc.w SME_mR7af_1C2-PLC_SStageTails
-		dc.w SME_mR7af_1CA-PLC_SStageTails
-		dc.w SME_mR7af_1D0-PLC_SStageTails
-		dc.w SME_mR7af_1D4-PLC_SStageTails
-		dc.w SME_mR7af_1D8-PLC_SStageTails
+		dc.w PLC_SStageTails0-PLC_SStageTails
+		dc.w PLC_SStageTails1-PLC_SStageTails
+		dc.w PLC_SStageTails2-PLC_SStageTails
+		dc.w PLC_SStageTails3-PLC_SStageTails
+		dc.w PLC_SStageTails4-PLC_SStageTails
+		dc.w PLC_SStageTails5-PLC_SStageTails
+		dc.w PLC_SStageTails0-PLC_SStageTails
+		dc.w PLC_SStageTails1-PLC_SStageTails
+		dc.w PLC_SStageTails2-PLC_SStageTails
+		dc.w PLC_SStageTails3-PLC_SStageTails
+		dc.w PLC_SStageTails4-PLC_SStageTails
+		dc.w PLC_SStageTails5-PLC_SStageTails
+		dc.w PLC_SStageTailsC-PLC_SStageTails
+		dc.w PLC_SStageTailsD-PLC_SStageTails
+		dc.w PLC_SStageTailsE-PLC_SStageTails
+		dc.w PLC_SStageTailsF-PLC_SStageTails
 PLC_SStageKnuckles:
-		dc.w SME_mR7af_1DC-PLC_SStageKnuckles
-		dc.w SME_mR7af_1E2-PLC_SStageKnuckles
-		dc.w SME_mR7af_1EA-PLC_SStageKnuckles
-		dc.w SME_mR7af_1F2-PLC_SStageKnuckles
-		dc.w SME_mR7af_1FA-PLC_SStageKnuckles
-		dc.w SME_mR7af_202-PLC_SStageKnuckles
-		dc.w SME_mR7af_20A-PLC_SStageKnuckles
-		dc.w SME_mR7af_210-PLC_SStageKnuckles
-		dc.w SME_mR7af_218-PLC_SStageKnuckles
-		dc.w SME_mR7af_220-PLC_SStageKnuckles
-		dc.w SME_mR7af_228-PLC_SStageKnuckles
-		dc.w SME_mR7af_230-PLC_SStageKnuckles
-		dc.w SME_mR7af_238-PLC_SStageKnuckles
-		dc.w SME_mR7af_240-PLC_SStageKnuckles
-		dc.w SME_mR7af_244-PLC_SStageKnuckles
-		dc.w SME_mR7af_248-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles0-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles1-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles2-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles3-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles4-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles5-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles0-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles1-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles2-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles3-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles4-PLC_SStageKnuckles
+		dc.w PLC_SStageKnuckles5-PLC_SStageKnuckles
+		dc.w PLC_SStageKnucklesC-PLC_SStageKnuckles
+		dc.w PLC_SStageKnucklesD-PLC_SStageKnuckles
+		dc.w PLC_SStageSonicE-PLC_SStageKnuckles
+		dc.w PLC_SStageKnucklesF-PLC_SStageKnuckles
 PLC_SStageAmy:
-		dc.w SME_mR7af_24C-PLC_SStageAmy
-		dc.w SME_mR7af_252-PLC_SStageAmy
-		dc.w SME_mR7af_25A-PLC_SStageAmy
-		dc.w SME_mR7af_262-PLC_SStageAmy
-		dc.w SME_mR7af_26A-PLC_SStageAmy
-		dc.w SME_mR7af_272-PLC_SStageAmy
-		dc.w SME_mR7af_27A-PLC_SStageAmy
-		dc.w SME_mR7af_280-PLC_SStageAmy
-		dc.w SME_mR7af_288-PLC_SStageAmy
-		dc.w SME_mR7af_290-PLC_SStageAmy
-		dc.w SME_mR7af_298-PLC_SStageAmy
-		dc.w SME_mR7af_2A0-PLC_SStageAmy
-		dc.w SME_mR7af_2A8-PLC_SStageAmy
-		dc.w SME_mR7af_2AE-PLC_SStageAmy
-		dc.w SME_mR7af_2B2-PLC_SStageAmy
-		dc.w SME_mR7af_2B6-PLC_SStageAmy
+		dc.w PLC_SStageSonic0-PLC_SStageAmy
+		dc.w PLC_SStageSonic1-PLC_SStageAmy
+		dc.w PLC_SStageSonic2-PLC_SStageAmy
+		dc.w PLC_SStageSonic3-PLC_SStageAmy
+		dc.w PLC_SStageSonic4-PLC_SStageAmy
+		dc.w PLC_SStageSonic5-PLC_SStageAmy
+		dc.w PLC_SStageSonic0-PLC_SStageAmy
+		dc.w PLC_SStageSonic1-PLC_SStageAmy
+		dc.w PLC_SStageSonic2-PLC_SStageAmy
+		dc.w PLC_SStageSonic3-PLC_SStageAmy
+		dc.w PLC_SStageSonic4-PLC_SStageAmy
+		dc.w PLC_SStageSonic5-PLC_SStageAmy
+		dc.w PLC_SStageSonicC-PLC_SStageAmy
+		dc.w PLC_SStageSonicD-PLC_SStageAmy
+		dc.w PLC_SStageSonicE-PLC_SStageAmy
+		dc.w PLC_SStageSonicF-PLC_SStageAmy
 PLC_SStageMighty:
-		dc.w SME_mR7af_2BA-PLC_SStageMighty
-		dc.w SME_mR7af_2C0-PLC_SStageMighty
-		dc.w SME_mR7af_2C8-PLC_SStageMighty
-		dc.w SME_mR7af_2D0-PLC_SStageMighty
-		dc.w SME_mR7af_2D8-PLC_SStageMighty
-		dc.w SME_mR7af_2E0-PLC_SStageMighty
-		dc.w SME_mR7af_2E8-PLC_SStageMighty
-		dc.w SME_mR7af_2EE-PLC_SStageMighty
-		dc.w SME_mR7af_2F6-PLC_SStageMighty
-		dc.w SME_mR7af_2FE-PLC_SStageMighty
-		dc.w SME_mR7af_306-PLC_SStageMighty
-		dc.w SME_mR7af_30E-PLC_SStageMighty
-		dc.w SME_mR7af_316-PLC_SStageMighty
-		dc.w SME_mR7af_31C-PLC_SStageMighty
-		dc.w SME_mR7af_320-PLC_SStageMighty
-		dc.w SME_mR7af_324-PLC_SStageMighty
+		dc.w PLC_SStageSonic0-PLC_SStageMighty
+		dc.w PLC_SStageSonic1-PLC_SStageMighty
+		dc.w PLC_SStageSonic2-PLC_SStageMighty
+		dc.w PLC_SStageSonic3-PLC_SStageMighty
+		dc.w PLC_SStageSonic4-PLC_SStageMighty
+		dc.w PLC_SStageSonic5-PLC_SStageMighty
+		dc.w PLC_SStageSonic0-PLC_SStageMighty
+		dc.w PLC_SStageSonic1-PLC_SStageMighty
+		dc.w PLC_SStageSonic2-PLC_SStageMighty
+		dc.w PLC_SStageSonic3-PLC_SStageMighty
+		dc.w PLC_SStageSonic4-PLC_SStageMighty
+		dc.w PLC_SStageSonic5-PLC_SStageMighty
+		dc.w PLC_SStageSonicC-PLC_SStageMighty
+		dc.w PLC_SStageSonicD-PLC_SStageMighty
+		dc.w PLC_SStageSonicE-PLC_SStageMighty
+		dc.w PLC_SStageSonicF-PLC_SStageMighty
 PLC_SStageRay:
-		dc.w SME_mR7af_328-PLC_SStageRay
-		dc.w SME_mR7af_32E-PLC_SStageRay
-		dc.w SME_mR7af_336-PLC_SStageRay
-		dc.w SME_mR7af_33E-PLC_SStageRay
-		dc.w SME_mR7af_346-PLC_SStageRay
-		dc.w SME_mR7af_34E-PLC_SStageRay
-		dc.w SME_mR7af_356-PLC_SStageRay
-		dc.w SME_mR7af_35C-PLC_SStageRay
-		dc.w SME_mR7af_364-PLC_SStageRay
-		dc.w SME_mR7af_36C-PLC_SStageRay
-		dc.w SME_mR7af_374-PLC_SStageRay
-		dc.w SME_mR7af_37C-PLC_SStageRay
-		dc.w SME_mR7af_384-PLC_SStageRay
-		dc.w SME_mR7af_38A-PLC_SStageRay
-		dc.w SME_mR7af_38E-PLC_SStageRay
-		dc.w SME_mR7af_392-PLC_SStageRay
+		dc.w PLC_SStageSonic0-PLC_SStageRay
+		dc.w PLC_SStageSonic1-PLC_SStageRay
+		dc.w PLC_SStageSonic2-PLC_SStageRay
+		dc.w PLC_SStageSonic3-PLC_SStageRay
+		dc.w PLC_SStageSonic4-PLC_SStageRay
+		dc.w PLC_SStageSonic5-PLC_SStageRay
+		dc.w PLC_SStageSonic0-PLC_SStageRay
+		dc.w PLC_SStageSonic1-PLC_SStageRay
+		dc.w PLC_SStageSonic2-PLC_SStageRay
+		dc.w PLC_SStageSonic3-PLC_SStageRay
+		dc.w PLC_SStageSonic4-PLC_SStageRay
+		dc.w PLC_SStageSonic5-PLC_SStageRay
+		dc.w PLC_SStageSonicC-PLC_SStageRay
+		dc.w PLC_SStageSonicD-PLC_SStageRay
+		dc.w PLC_SStageSonicE-PLC_SStageRay
+		dc.w PLC_SStageSonicF-PLC_SStageRay
 PLC_SStageMetalSonic:
-		dc.w SME_mR7af_396-PLC_SStageMetalSonic
-		dc.w SME_mR7af_39C-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3A4-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3AC-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3B4-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3BC-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3C4-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3CA-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3D2-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3DA-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3E2-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3EA-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3F2-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3F8-PLC_SStageMetalSonic
-		dc.w SME_mR7af_3FC-PLC_SStageMetalSonic
-		dc.w SME_mR7af_400-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic0-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic1-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic2-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic3-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic4-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic5-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic0-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic1-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic2-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic3-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic4-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonic5-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonicC-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonicD-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonicE-PLC_SStageMetalSonic
+		dc.w PLC_SStageSonicF-PLC_SStageMetalSonic
 PLC_SStageTailstails:
-		dc.w SME_mR7af_404-PLC_SStageTailstails
-		dc.w SME_mR7af_408-PLC_SStageTailstails
-		dc.w SME_mR7af_40C-PLC_SStageTailstails
-		dc.w SME_mR7af_410-PLC_SStageTailstails
-		dc.w SME_mR7af_414-PLC_SStageTailstails
-		dc.w SME_mR7af_418-PLC_SStageTailstails
-		dc.w SME_mR7af_41C-PLC_SStageTailstails
-		dc.w SME_mR7af_420-PLC_SStageTailstails
-		dc.w SME_mR7af_424-PLC_SStageTailstails
-		dc.w SME_mR7af_428-PLC_SStageTailstails
-		dc.w SME_mR7af_42C-PLC_SStageTailstails
-		dc.w SME_mR7af_430-PLC_SStageTailstails
-		dc.w SME_mR7af_434-PLC_SStageTailstails
-		dc.w SME_mR7af_438-PLC_SStageTailstails
-SME_mR7af_FC:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_102:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_10A:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_112:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_11A:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_122:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_12A:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_130:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_138:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_140:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_148:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_150:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_158:	dc.b 0, 2, $F0, $59, $20, $69
-SME_mR7af_15E:	dc.b 0, 1, $F0, $6C
-SME_mR7af_162:	dc.b 0, 1, $F0, $7C
-SME_mR7af_166:	dc.b 0, 1, $F0, $8C
-SME_mR7af_16A:	dc.b 0, 3, $30, $9C, $B0, $A0, $20, $AC
-SME_mR7af_172:	dc.b 0, 3, $30, $AF, $B0, $B3, $10, $BF
-SME_mR7af_17A:	dc.b 0, 3, $B0, $C1, $20, $CD, $10, $D0
-SME_mR7af_182:	dc.b 0, 3, $30, $D2, $B0, $D6, 0, $E2
-SME_mR7af_18A:	dc.b 0, 3, $B0, $C1, $20, $CD, $10, $D0
-SME_mR7af_192:	dc.b 0, 3, $30, $AF, $B0, $B3, $10, $BF
-SME_mR7af_19A:	dc.b 0, 3, $30, $9C, $B0, $A0, $20, $AC
-SME_mR7af_1A2:	dc.b 0, 3, $30, $AF, $B0, $B3, $10, $BF
-SME_mR7af_1AA:	dc.b 0, 3, $B0, $C1, $20, $CD, $10, $D0
-SME_mR7af_1B2:	dc.b 0, 3, $30, $D2, $B0, $D6, 0, $E2
-SME_mR7af_1BA:	dc.b 0, 3, $B0, $C1, $20, $CD, $10, $D0
-SME_mR7af_1C2:	dc.b 0, 3, $30, $AF, $B0, $B3, $10, $BF
-SME_mR7af_1CA:	dc.b 0, 2, $30, $E3, $B0, $E7
-SME_mR7af_1D0:	dc.b 0, 1, $B0, $F3
-SME_mR7af_1D4:	dc.b 0, 1, $B0, $FF
-SME_mR7af_1D8:	dc.b 0, 1, $B1, $B
-SME_mR7af_1DC:	dc.b 0, 2, $F1, $17, $71, $27
-SME_mR7af_1E2:	dc.b 0, 3, $F1, $2F, $31, $3F, $11, $43
-SME_mR7af_1EA:	dc.b 0, 3, $F1, $45, $21, $55, $31, $58
-SME_mR7af_1F2:	dc.b 0, 3, $F1, $5C, $21, $6C, 1, $6F
-SME_mR7af_1FA:	dc.b 0, 3, $F1, $45, $21, $55, $31, $58
-SME_mR7af_202:	dc.b 0, 3, $F1, $2F, $31, $3F, $11, $43
-SME_mR7af_20A:	dc.b 0, 2, $F1, $17, $71, $27
-SME_mR7af_210:	dc.b 0, 3, $F1, $2F, $31, $3F, $11, $43
-SME_mR7af_218:	dc.b 0, 3, $F1, $45, $21, $55, $31, $58
-SME_mR7af_220:	dc.b 0, 3, $F1, $5C, $21, $6C, 1, $6F
-SME_mR7af_228:	dc.b 0, 3, $F1, $45, $21, $55, $31, $58
-SME_mR7af_230:	dc.b 0, 3, $F1, $2F, $31, $3F, $11, $43
-SME_mR7af_238:	dc.b 0, 3, $F1, $70, $31, $80, $11, $84
-SME_mR7af_240:	dc.b 0, 1, $F1, $86
-SME_mR7af_244:	dc.b 0, 1, $F0, $7C
-SME_mR7af_248:	dc.b 0, 1, $F1, $96
-SME_mR7af_24C:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_252:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_25A:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_262:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_26A:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_272:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_27A:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_280:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_288:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_290:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_298:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_2A0:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_2A8:	dc.b 0, 2, $F0, $59, $20, $69
-SME_mR7af_2AE:	dc.b 0, 1, $F0, $6C
-SME_mR7af_2B2:	dc.b 0, 1, $F0, $7C
-SME_mR7af_2B6:	dc.b 0, 1, $F0, $8C
-SME_mR7af_2BA:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_2C0:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_2C8:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_2D0:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_2D8:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_2E0:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_2E8:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_2EE:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_2F6:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_2FE:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_306:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_30E:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_316:	dc.b 0, 2, $F0, $59, $20, $69
-SME_mR7af_31C:	dc.b 0, 1, $F0, $6C
-SME_mR7af_320:	dc.b 0, 1, $F0, $7C
-SME_mR7af_324:	dc.b 0, 1, $F0, $8C
-SME_mR7af_328:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_32E:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_336:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_33E:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_346:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_34E:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_356:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_35C:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_364:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_36C:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_374:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_37C:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_384:	dc.b 0, 2, $F0, $59, $20, $69
-SME_mR7af_38A:	dc.b 0, 1, $F0, $6C
-SME_mR7af_38E:	dc.b 0, 1, $F0, $7C
-SME_mR7af_392:	dc.b 0, 1, $F0, $8C
-SME_mR7af_396:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_39C:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_3A4:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_3AC:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_3B4:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_3BC:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_3C4:	dc.b 0, 2, $F0, 0, $50, $10
-SME_mR7af_3CA:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_3D2:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_3DA:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
-SME_mR7af_3E2:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
-SME_mR7af_3EA:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
-SME_mR7af_3F2:	dc.b 0, 2, $F0, $59, $20, $69
-SME_mR7af_3F8:	dc.b 0, 1, $F0, $6C
-SME_mR7af_3FC:	dc.b 0, 1, $F0, $7C
-SME_mR7af_400:	dc.b 0, 1, $F0, $8C
-SME_mR7af_404:	dc.b 0, 1, $51, $A6
-SME_mR7af_408:	dc.b 0, 1, $81, $AC
-SME_mR7af_40C:	dc.b 0, 1, $51, $B5
-SME_mR7af_410:	dc.b 0, 1, $51, $BB
-SME_mR7af_414:	dc.b 0, 1, $71, $C1
-SME_mR7af_418:	dc.b 0, 1, $81, $C9
-SME_mR7af_41C:	dc.b 0, 1, $51, $D2
-SME_mR7af_420:	dc.b 0, 1, $51, $A6
-SME_mR7af_424:	dc.b 0, 1, $81, $AC
-SME_mR7af_428:	dc.b 0, 1, $51, $B5
-SME_mR7af_42C:	dc.b 0, 1, $51, $BB
-SME_mR7af_430:	dc.b 0, 1, $71, $C1
-SME_mR7af_434:	dc.b 0, 1, $81, $C9
-SME_mR7af_438:	dc.b 0, 1, $51, $D2
+		dc.w PLC_SStageTailstails0-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails1-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails2-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails3-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails4-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails5-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails6-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails0-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails1-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails2-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails3-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails4-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails5-PLC_SStageTailstails
+		dc.w PLC_SStageTailstails6-PLC_SStageTailstails
+PLC_SStageSonic0:	dc.b 0, 2, $F0, 0, $50, $10
+PLC_SStageSonic1:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
+PLC_SStageSonic2:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
+PLC_SStageSonic3:	dc.b 0, 3, $F0, $44, $20, $54, $10, $57
+PLC_SStageSonic4:	dc.b 0, 3, $F0, $2D, $20, $3D, $30, $40
+PLC_SStageSonic5:	dc.b 0, 3, $F0, $16, $20, $26, $30, $29
+PLC_SStageSonicC:	dc.b 0, 2, $F0, $59, $20, $69
+PLC_SStageSonicD:	dc.b 0, 1, $F0, $6C
+PLC_SStageSonicE:	dc.b 0, 1, $F0, $7C
+PLC_SStageSonicF:	dc.b 0, 1, $F0, $8C
+PLC_SStageTails0:	dc.b 0, 3, $30, $9C, $B0, $A0, $20, $AC
+PLC_SStageTails1:	dc.b 0, 3, $30, $AF, $B0, $B3, $10, $BF
+PLC_SStageTails2:	dc.b 0, 3, $20, $C1, $B0, $C4, $10, $D0
+PLC_SStageTails3:	dc.b 0, 3, $30, $D2, $B0, $D6, 0, $E2
+PLC_SStageTails4:	dc.b 0, 3, $20, $C1, $B0, $C4, $10, $D0
+PLC_SStageTails5:	dc.b 0, 3, $30, $AF, $B0, $B3, $10, $BF
+PLC_SStageTailsC:	dc.b 0, 2, $30, $E3, $B0, $E7
+PLC_SStageTailsD:	dc.b 0, 1, $B0, $F3
+PLC_SStageTailsE:	dc.b 0, 1, $B0, $FF
+PLC_SStageTailsF:	dc.b 0, 1, $B1, $B
+PLC_SStageKnuckles0:	dc.b 0, 2, $F1, $17, $51, $27
+PLC_SStageKnuckles1:	dc.b 0, 3, $F1, $2D, $21, $3D, $11, $40
+PLC_SStageKnuckles2:	dc.b 0, 3, $F1, $42, $21, $52, $31, $55
+PLC_SStageKnuckles3:	dc.b 0, 3, $F1, $59, $21, $69, 1, $6C
+PLC_SStageKnuckles4:	dc.b 0, 3, $F1, $42, $21, $52, $31, $55
+PLC_SStageKnuckles5:	dc.b 0, 3, $F1, $2D, $21, $3D, $11, $40
+PLC_SStageKnucklesC:	dc.b 0, 2, $B1, $6D, $51, $79
+PLC_SStageKnucklesD:	dc.b 0, 1, $F1, $7F
+PLC_SStageKnucklesF:	dc.b 0, 1, $F1, $8F
+PLC_SStageTailstails0:	dc.b 0, 1, $51, $9F
+PLC_SStageTailstails1:	dc.b 0, 1, $81, $A5
+PLC_SStageTailstails2:	dc.b 0, 1, $51, $AE
+PLC_SStageTailstails3:	dc.b 0, 1, $51, $B4
+PLC_SStageTailstails4:	dc.b 0, 1, $71, $BA
+PLC_SStageTailstails5:	dc.b 0, 1, $81, $C2
+PLC_SStageTailstails6:	dc.b 0, 1, $51, $CB
