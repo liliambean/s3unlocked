4803d778351f4947f30d9f1c5831a8702b6b018d
Liliam Bronze <liliambronze@gmail.com>
2025-05-23 12:32:34 +0100

    Add boss indicator to level select

--- General/Save Menu/Map - Level Select.asm
+++ General/Save Menu/Map - Level Select.asm
@@ -6,10 +6,11 @@
 		dc.w Map_LevelSelectRay-Map_LevelSelect
 		dc.w Map_LevelSelectMetalSonic-Map_LevelSelect
 		dc.w Map_LevelSelectZoneIcon-Map_LevelSelect
-		dc.w Map_LevelSelectArrow_LeftSide-Map_LevelSelect
-		dc.w Map_LevelSelectArrow_RightSide-Map_LevelSelect
+		dc.w Map_LevelSelectBossIcon-Map_LevelSelect
 		dc.w Map_LevelSelectPlayer_1P-Map_LevelSelect
 		dc.w Map_LevelSelectPlayer_2P-Map_LevelSelect
+		dc.w Map_LevelSelectArrow_LeftArrow-Map_LevelSelect
+		dc.w Map_LevelSelectArrow_RightArrow-Map_LevelSelect
 Map_LevelSelectSonic:
 		dc.w 1
 		dc.b  $F8,   5, $25, $E4, $FF, $F8
@@ -39,12 +40,22 @@ Map_LevelSelectZoneIcon:
 		dc.b    4,  $E, $60, $60, $FF, $D8
 		dc.b    4,  $E, $60, $6C, $FF, $F8
 		dc.b    4,   6, $60, $78,   0, $18
-Map_LevelSelectArrow_LeftSide:
-		dc.w 1
-		dc.b  $F8,   1,   5, $E2, $FF, $FC
-Map_LevelSelectArrow_RightSide:
-		dc.w 1
-		dc.b  $F8,   1,  $D, $E2, $FF, $FA
+Map_LevelSelectBossIcon:
+		dc.w $E
+		dc.b  $E4,  $F, $60, $38, $FF, $D8
+		dc.b  $E4,  $F, $60, $48, $FF, $F8
+		dc.b  $E4,   7, $60, $58,   0, $18
+		dc.b    4,  $E, $60, $60, $FF, $D8
+		dc.b    4,   0, $60, $6C, $FF, $F8
+		dc.b    4,   0, $60, $6F,   0,   0
+		dc.b    4,   0, $60, $72,   0,   8
+		dc.b    4,   0, $60, $75,   0, $10
+		dc.b    4,   0, $60, $78,   0, $18
+		dc.b    4,   0, $60, $7B,   0, $20
+		dc.b  $11,   0, $40, $1F,   0,   1
+		dc.b  $11,   0, $40, $2C,   0,   9
+		dc.b  $11,   0, $40, $30,   0, $11
+		dc.b  $11,   0, $40, $30,   0, $19
 Map_LevelSelectPlayer_1P:
 		dc.w 2
 		dc.b  $F0,   0, $40, $11, $FF, $F8
@@ -53,3 +64,9 @@ Map_LevelSelectPlayer_2P:
 		dc.w 2
 		dc.b  $F0,   0, $40, $12, $FF, $F8
 		dc.b  $F0,   0, $40, $2D,   0,   0
+Map_LevelSelectArrow_LeftArrow:
+		dc.w 1
+		dc.b  $F8,   1,  $D, $E2, $FF, $FA
+Map_LevelSelectArrow_RightArrow:
+		dc.w 1
+		dc.b  $F8,   1,   5, $E2, $FF, $FC
