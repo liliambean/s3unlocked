007ade4dae1a05176ea02304686f761ef0d47744
Liliam Bronze <liliambronze@gmail.com>
2025-05-29 14:04:12 +0100

    more wip

---
+++ General/Sprites/Ring/Map - Combine Ring.asm
@@ -0,0 +1,37 @@
+		dc.w Map_CombineRing0-Map_CombineRing
+		dc.w Map_CombineRing0-Map_CombineRing
+		dc.w Map_CombineRing0-Map_CombineRing
+		dc.w Map_CombineRing3-Map_CombineRing
+		dc.w Map_CombineRing4-Map_CombineRing
+		dc.w Map_CombineRing5-Map_CombineRing
+		dc.w Map_CombineRing6-Map_CombineRing
+		dc.w Map_CombineRing6-Map_CombineRing
+		dc.w Map_CombineRing8-Map_CombineRing
+		dc.w Map_CombineRing9-Map_CombineRing
+Map_CombineRing0:
+		dc.w 1
+		dc.b  $F4,  $A,   0,   0, $FF, $F4
+Map_CombineRing3:
+		dc.w 1
+		dc.b  $F4,   6,   0,   0, $FF, $F8
+Map_CombineRing4:
+		dc.w 1
+		dc.b  $F4,   2,   0,   0, $FF, $FC
+Map_CombineRing5:
+		dc.w 1
+		dc.b  $F4,   6,   8,   0, $FF, $F8
+Map_CombineRing6:
+		dc.w 1
+		dc.b  $F4,  $A,   8,   0, $FF, $F4
+Map_CombineRing8:
+		dc.w 4
+		dc.b  $F3,   5, $18,   4, $FF, $F4
+		dc.b  $F3,   5, $10,   4, $FF, $FB
+		dc.b  $FC,   5,   8,   4, $FF, $F4
+		dc.b  $FC,   5,   0,   4, $FF, $FB
+Map_CombineRing9:
+		dc.w 4
+		dc.b  $F3,   1,   0,   4, $FF, $F3
+		dc.b  $F3,   5,   8,   4, $FF, $FC
+		dc.b  $FC,   5, $10,   4, $FF, $F3
+		dc.b  $FC,   1, $18,   4,   0,   4
