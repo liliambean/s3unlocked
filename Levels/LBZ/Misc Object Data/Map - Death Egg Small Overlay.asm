c5bc8f8417e38fb6490529a96e0ffe8913beafe6
Liliam Bronze <liliambronze@gmail.com>
2025-07-11 00:59:39 +0100

    pain

---
+++ Levels/LBZ/Misc Object Data/Map - Death Egg Small Overlay.asm
@@ -0,0 +1,11 @@
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 3 & Knuckles format
+; --------------------------------------------------------------------------------
+
+SME_ryqpU:	
+		dc.w SME_ryqpU_2-SME_ryqpU	
+SME_ryqpU_2:	dc.b 0, 3	
+		dc.b 0, 4, 0, 0, $FF, $E0	
+		dc.b 0, 9, 0, 2, $FF, $F0	
+		dc.b $E8, 7, 0, 8, 0, 8	
+		even
